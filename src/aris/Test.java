64eb9865266e8b138d18bd8eaa07ec848e578440
poonam <poomuk17@gmail.com>
2017-11-14 14:19:39 +0000

    updated test.java
     Ptease enter the commit message for your changes. Lines starting

--- src/aris/Test.java
+++ src/aris/Test.java
@@ -1,10 +1,19 @@
 package aris;
 
 public class Test {
-
+	 public static int sum(int x) 
+	 {
+		 int add = 0;
+		 int k = 0;
+		 while(k < x) 
+		 { 
+			 add += k;
+			 k++; 
+		 }
+		 return add; 
+	 } 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
 	}
 
 }
